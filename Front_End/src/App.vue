9eab959511ee3997ab52da0e52df435f19308182
RobbenLe <144344546+RobbenLe@users.noreply.github.com>
2025-03-20 21:40:52 +0100

    Set Up Working Environment

---
+++ Front_End/src/App.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    double() {
+      return this.count * 2;
+    },
+    countArray() {
+      const numbers = [];
+      for (let i = 0; i < this.count; i++) {
+        numbers.push(i);
+      }
+      return numbers;
+    },
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    currentTime() {
+      return new Date();
+    },
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`);
+  },
+};
+</script>
+<template>
+  <!-- handle an event, alternate syntax v-on -->
+  <button @click="increment">Count is: {{ count }}</button>
+  <!-- use a computed property -->
+  <p>Twice the count is: {{ double }}</p>
+  <!-- call a method -->
+  <p>The current date and time are: {{ currentTime() }}</p>
+  <!-- use a conditional -->
+  <p v-if="count % 2 === 0">The number is even</p>
+  <p v-else>The number is odd</p>
+  <p>Number count:</p>
+  <!-- loop through an array, countArray is a computed property returning an array -->
+  <div v-for="number in countArray">
+    {{ number }}
+  </div>
+</template>
+<style lang="css" scoped>
+button {
+  background-color: blue;
+  color: white;
+}
+</style>
